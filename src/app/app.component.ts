e9350fd351ecd24e52206ca1f00f5779cda63584
Robert Inostroza <rinostrozaj@Mac-Pro-de-Rovert.local>
2025-07-14 23:03:46 -0400

    Primer commit del proyecto Sonido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationCancel, NavigationEnd } from '@angular/router';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { filter } from 'rxjs/operators';
+declare let $: any;
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [
+        Location, {
+            provide: LocationStrategy,
+            useClass: PathLocationStrategy
+        }
+    ]
 })
-export class AppComponent {
-  title = 'Sonido';
+export class AppComponent implements OnInit {
+    location: any;
+    routerSubscription: any;
+
+    constructor(private router: Router) {
+    }
+
+    ngOnInit(){
+        this.recallJsFuntions();
+    }
+
+    recallJsFuntions() {
+        this.router.events
+        .subscribe((event) => {
+            if ( event instanceof NavigationStart ) {
+                $('.preloader').fadeIn('slow');
+            }
+        });
+        this.routerSubscription = this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel))
+        .subscribe(event => {
+            $.getScript('../assets/js/custom.js');
+            $('.preloader').fadeOut('slow');
+            this.location = this.router.url;
+            if (!(event instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
+    }
 }
