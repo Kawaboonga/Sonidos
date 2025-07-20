f54e97503b59c19c56e04ea00ef3cda108ee2d23
Robert Inostroza <rinostrozaj@Mac-Pro-de-Rovert.local>
2025-07-20 00:38:22 -0400

    Primer commit del proyecto Sonidos

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/nuva-ng'),
+      dir: require('path').join(__dirname, './coverage/sonidos-ng'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
